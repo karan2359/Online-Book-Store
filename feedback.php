e754a5542e171a887ecc421135f8827abbe222f1
karan2359 <pardeshikaran060@gmail.com>
2026-02-18 17:27:55 +0530

    feedback Page

--- feedback.php
+++ feedback.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&amp;display=swap" rel="stylesheet">
     <title>Feedback</title>
+    <link rel="stylesheet" href="CSS/feedback.css">
 </head>
 <body>
     <header>
@@ -19,11 +20,41 @@
             </div>
         </nav>
     </header>
-    <form action="index.php" method="post">
-        <input type="text" name="name" id="name">
-        <input type="tel">
-        <input type="email" name="email" id="email">
-        
-    </form>
-</body>
-</html>
+    <div class="container">
+        <h2></h2>
+        <form action="index.php" method="post">
+            <input type="text" name="name" id="name" placeholder="Name" required>
+            <input type="tel" name="mobile" id="mobile" placeholder="Mobile No." required>
+            <input type="email" name="email" id="email" placeholder="Email" required>
+            <select name="feedback" id="feedback" required> 
+                <option value="#">Select</option>
+                <option value="Bad">Bad</option>
+                <option value="Not Bad">Not Bad</option>
+                <option value="Good">Good</option>
+                <!-- <option value="Impressive">Impressive</option> -->
+                <option value="Excellent">Excellent</option>
+            </select>
+            <textarea name="textarea" id="textarea" placeholder="Write The Feedback" required></textarea>
+            <button type="submit">Submit</button>
+            <button type="reset">Clear</button>
+            
+        </form>
+    </div>
+    </body>
+    </html>
+
+    <?php
+include_once 'config.php';
+
+
+$success_msg = '';
+$error_msg = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $fullname = trim($_POST['fullname']);
+    $email = trim($_POST['email']);
+    $mobile = trim($_POST['mobile']);
+    $feedback =($_POST['feedback']);
+    $textarea = trim($_POST['textarea']);
+}
+?>
